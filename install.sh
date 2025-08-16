1b9d9af3cd421c35dd8a3ef7cb5429b562b83115
xiaochun <90xchun@gmail.com>
2025-08-16 11:56:02 +0800

    add

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 configFile='config/base-config.properties'
+timezone='Asia/Chongqing'
 
 prop() {
     grep "${1}" "${configFile}"|awk -F ${1}'=' '{print $2}'
 }
 
-docker run -d -p $(prop 'expose.port'):$(prop 'server.port') \
+docker run -d -p $(prop 'expose.port'):$(prop 'server.port') -e TZ=${timezone} \
  -e APP_NAME="$(prop 'app.name')" \
  -e EXPOSE_PORT="$(prop 'expose.port')" \
  -e DOCKER_MODE='true' \
